b7add0b0b5ca9791e765583b472947f9f2285500
vicentepf78 <vicentepf78.gmail.com>
2021-06-05 11:36:03 -0300

    alteracoes modulo 1 e 2 client

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,117 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 
-campo.on("input", function() {
 
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);    
 
 });
+
+function atualizaTamanhoFrase(params) {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+
+function inicializaContadores() {
+    campo.on("input", function() {
+    
+        var conteudo = campo.val();
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+    
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    
+    });
+}
+
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        $("#botao-reiniciar").attr("disabled",true);
+
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+    
+            if(tempoRestante < 1) {
+                clearInterval(cronometroID);
+                finalizaJogo();
+            }
+            
+        }, 1000);
+    });
+}
+
+function finalizaJogo() {
+    campo.attr("disabled", true);
+    $("#botao-reiniciar").attr("disabled",false);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
+
+
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function () {
+        
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
+    
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+    
+} 
+
+
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-verde");
+    campo.removeClass("borda-vermelha");
+}
+
+
+
+// Exercicios 
+
+function exercicio0301() {
+    var mae = $("#nome-mae");
+    var pai = $("#nome-pai");
+    
+    mae.on("input", function() {
+        pai.attr("disabled", true);
+    });
+    
+    pai.on("input", function() {
+        mae.attr("disabled", true);
+    });
+}
+
+function exercicio0303() {
+    $(".titulo").text("Meu primeiro blog");
+    $("#autor").val("Daniel");
+    $("textarea").attr("cols", 40);
+    $("textarea").attr("rows", 10);
+}
+
---
+++ public/js/placar.js
@@ -0,0 +1,61 @@
+$("#botao-placar").click(mostraPlacar);
+
+
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Vicente";
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.prepend(linha);       
+    $(".placar").slideDown(500);    
+    scrollPlacar();      
+}
+
+function scrollPlacar() {
+    var posicaoPlacar = $(".placar").offset().top;
+    console.log(posicaoPlacar);
+
+    $("html, body").animate(
+    {
+        scrollTop: posicaoPlacar
+    },1000);
+}
+
+
+function novaLinha(usuario, numPalavras) {
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(numPalavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+
+}
+
+function removeLinha(event) {
+        event.preventDefault();
+        var linha = $(this).parent().parent();
+        linha.fadeOut(1000);
+        setTimeout(function() {
+            linha.remove();
+        },1000);
+}
+
+function mostraPlacar() {
+    $(".placar").stop().slideToggle(600);
+}
+
